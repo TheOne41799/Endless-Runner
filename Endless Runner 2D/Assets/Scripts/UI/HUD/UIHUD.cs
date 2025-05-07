3a44b3991f8bd7b7c23a875976294be5001e79ce
DESKTOP-4PBISAO\Anudeep <anudeepramadugu@gmail.com>
2025-05-08 01:16:59 +0530

    UIHUD updated

--- Endless Runner 2D/Assets/Scripts/UI/HUD/UIHUD.cs
+++ Endless Runner 2D/Assets/Scripts/UI/HUD/UIHUD.cs
@@ -1,9 +1,12 @@
+using TMPro;
 using UnityEngine;
 
 namespace EndlessRunner.UI
 {
     public class UIHUD : MonoBehaviour
     {
+        [SerializeField] private TextMeshProUGUI scoreText;
 
+        public void UpdateScore(int score) => scoreText.text = score.ToString();
     }
 }
