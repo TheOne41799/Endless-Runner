0d69cb13a69fa2267633542e694305300072d43a
DESKTOP-4PBISAO\Anudeep <anudeepramadugu@gmail.com>
2025-05-07 18:30:02 +0530

    PlayerView prefab created

--- Endless Runner 2D/Assets/Scripts/Data/Player/PlayerData.cs
+++ Endless Runner 2D/Assets/Scripts/Data/Player/PlayerData.cs
@@ -1,3 +1,4 @@
+using EndlessRunner.Player;
 using UnityEngine;
 
 namespace EndlessRunner.Data
@@ -5,6 +6,10 @@ namespace EndlessRunner.Data
     [CreateAssetMenu(menuName = "Game Data/ Player Data", fileName = "Player Data")]
     public class PlayerData : ScriptableObject
     {
+        [SerializeField] private PlayerView playerViewPrefab;
+        [SerializeField] private Vector3 spawnPosition;
 
+        public PlayerView PlayerViewPrefab => playerViewPrefab;
+        public Vector3 SpawnPosition => spawnPosition;
     }
 }
